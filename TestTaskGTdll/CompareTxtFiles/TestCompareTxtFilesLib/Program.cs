1a5cb7d9237c8d9a02be9fd364cb50c3e7d69082
alfacentaur <alfacentaur@yandex.ru>
2021-03-22 14:13:00 +0300

    TestTaskGTdll
    TestTaskGTdll

---
+++ TestTaskGTdll/CompareTxtFiles/TestCompareTxtFilesLib/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CompareTxtFilesLib;
+
+namespace TestCompareTxtFilesLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            CompareClass compare = new CompareClass(args);
+            compare.StartComparing();
+
+        }
+    }
+}
