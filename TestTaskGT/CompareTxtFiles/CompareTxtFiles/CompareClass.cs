b880f08d82c0af659b08f0dfa8641cf583e86cd9
alfacentaur <alfacentaur@yandex.ru>
2021-03-29 12:02:25 +0300

    changedTestTask
    changedTestTask

--- TestTaskGT/CompareTxtFiles/CompareTxtFiles/CompareClass.cs
+++ TestTaskGT/CompareTxtFiles/CompareTxtFiles/CompareClass.cs
@@ -20,6 +20,12 @@ namespace CompareTxtFiles
             MODIFIED_FILE
         }
 
+        enum OptionOfImplementation
+        {
+            CUSTOM,
+            THIRD_PATY
+        }
+
         //NLog class instances
         private static Logger logger = LogManager.GetCurrentClassLogger();
 
@@ -31,11 +37,15 @@ namespace CompareTxtFiles
 
         List<string> _inputArgValues = new List<string>();
 
-        List<string>[] _differences = new List<string>[2] { new List<string>(), new List<string>() };
+        List<string>[] _differences = new List<string>[Enum.GetValues(typeof(OptionOfImplementation)).Length];
 
         //constructor
         public CompareClass(string[] args)
         {
+            _differences[(int) OptionOfImplementation.CUSTOM] = new List<string>();
+
+            _differences[(int) OptionOfImplementation.THIRD_PATY] = new List<string>();
+
             //check correctness ars and initial of list of input filenames
 
             //Check count of input args
@@ -68,6 +78,49 @@ namespace CompareTxtFiles
             }
         }
 
+        //findChangesInLine
+        string findChangesInLine(string sOrig, string sMod)
+        {
+            string changes = "";
+
+            string[] sOrigArr = sOrig.Split(' ');
+
+            string[] sModArr = sMod.Split(' ');
+
+            int sOrigArrLength = sOrigArr.Length;
+
+            int sModArrLength = sModArr.Length;
+
+            for ( int i = 0; i < sOrigArrLength && i < sModArrLength; i++ )
+            {
+                if (sOrigArr[i].CompareTo(sModArr[i]) != 0)
+                {
+                    changes = string.Concat(changes, "Word #", i + 1, " old: " , sOrigArr[i], " new: ", sModArr[i],"; ");
+                }
+            }
+
+            int difference = sOrigArrLength - sModArrLength;
+
+            if (difference < 0)
+            {
+                changes = string.Concat(changes, "Added " + Math.Abs(difference) + " word(s) from the end:");
+                for( int i = sOrigArrLength; i < sOrigArrLength + Math.Abs(difference); i++)
+                {
+                    changes = string.Concat(changes, " ", sModArr[i]);
+                }
+            } 
+            else if (difference > 0)
+            {
+                changes = string.Concat(changes, "Deleted " + Math.Abs(difference) + " word(s) from the end:");
+                for (int i = sOrigArrLength - difference; i < sOrigArrLength; i++)
+                {
+                    changes = string.Concat(changes, " ", sOrigArr[i]);
+                }
+            }
+            return changes;
+        }
+
+        //temminate application
         void TerminateApp()
         {
             Environment.Exit(-1);
@@ -92,23 +145,23 @@ namespace CompareTxtFiles
             {
                 if (differResult.DiffBlocks[i].DeleteCountA == differResult.DiffBlocks[i].InsertCountB)
                 {
-                    _differences[1].Add(string.Concat(differResult.DiffBlocks[i].DeleteStartA + 1, " : <modified line value for line ", differResult.DiffBlocks[i].DeleteStartA + 1, ">"));
+                    _differences[(int) OptionOfImplementation.THIRD_PATY].Add(string.Concat(string.Concat("Line #", differResult.DiffBlocks[i].DeleteStartA + 1, " : Modifications: ", findChangesInLine(differResult.PiecesOld[i], differResult.PiecesNew[i]))));
                 }
                 else if (differResult.DiffBlocks[i].DeleteCountA > differResult.DiffBlocks[i].InsertCountB)
                 {
-                    _differences[1].Add(string.Concat("<deleted line(s) from ",
+                    _differences[(int)OptionOfImplementation.THIRD_PATY].Add(string.Concat("Deleted line(s) from ",
 
                     differResult.DiffBlocks[i].DeleteStartA + 1, " to ",
 
-                    differResult.DiffBlocks[i].DeleteStartA + differResult.DiffBlocks[i].DeleteCountA, ">"));
+                    differResult.DiffBlocks[i].DeleteStartA + differResult.DiffBlocks[i].DeleteCountA));
                 }
                 else if (differResult.DiffBlocks[i].DeleteCountA < differResult.DiffBlocks[i].InsertCountB)
                 {
-                    _differences[1].Add(string.Concat("<added line(s) from ",
+                    _differences[(int)OptionOfImplementation.THIRD_PATY].Add(string.Concat("Added line(s) from ",
 
                         differResult.DiffBlocks[i].InsertStartB + 1, " to ",
 
-                        differResult.DiffBlocks[i].InsertStartB + differResult.DiffBlocks[i].InsertCountB, ">"));
+                        differResult.DiffBlocks[i].InsertStartB + differResult.DiffBlocks[i].InsertCountB));
                 }
 
             }
@@ -120,16 +173,18 @@ namespace CompareTxtFiles
         {
             logger.Debug(string.Concat("STARTING APPLICATION AT ", DateTime.Now));
             OpenInputFilesAndCompare();
-            OutputResult("RESULT OF COMPARING BY CUSTOM:", _differences[0]);
+            OutputResult("RESULT OF COMPARING BY CUSTOM:", _differences[(int) OptionOfImplementation.CUSTOM]);
             Console.WriteLine();
-            OutputResult("RESULT OF COMPARING BY THIRD-PARTY LIB:", _differences[1]);
+            OutputResult("RESULT OF COMPARING BY THIRD-PARTY LIB:", _differences[(int) OptionOfImplementation.THIRD_PATY]);
         }
 
         //Comparing files by lines and write to list of defferences
         void Comparing()
         {
             var curLine = 1;
+
             string sOrig, sMod;
+
             while (
                     ((sOrig = _fileStreams[(int)MeansOfInputArgs.ORIGINAL_FILE].ReadLine()) != null) &&
                     ((sMod = _fileStreams[(int)MeansOfInputArgs.MODIFIED_FILE].ReadLine()) != null)
@@ -137,7 +192,7 @@ namespace CompareTxtFiles
             {
                 if (String.Compare(sOrig, sMod) != 0)
                 {
-                    _differences[0].Add(string.Concat(curLine, " : <modified line value for line ", curLine, ">"));
+                    _differences[(int)OptionOfImplementation.CUSTOM].Add(string.Concat("Line #", curLine, " : Modifications: ", findChangesInLine(sOrig, sMod)));
                 }
                 curLine++;
             }
@@ -182,13 +237,27 @@ namespace CompareTxtFiles
                     {
                         using (_fileStreams[(int)MeansOfInputArgs.MODIFIED_FILE] = new StreamReader(_inputArgValues[(int)MeansOfInputArgs.MODIFIED_FILE]))
                         {
-                            logger.Info("INPUT FILES ARE OPENED");
+                            try
+                            {
+                                logger.Info("INPUT FILES ARE OPENED");
 
-                            // comparing
-                            Comparing();
+                                // comparing
+                                Comparing();
 
-                            // Comparing By Third-Party Lib
-                            ComparingByThirdPartyLib();
+                                // Comparing By Third-Party Lib
+                                ComparingByThirdPartyLib();
+
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine($"ERROR: {e.Message}");
+
+                                logger.Error($"ERROR: {e.Message}");
+
+                                TerminateApp();
+
+                            }
+                            
                         }
                     }
                     catch
